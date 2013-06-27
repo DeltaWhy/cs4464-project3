1d9290282118a942b1e1079809eec701fba25b81
Michael Limiero <mike5713@gmail.com>
2013-06-27 23:17:01 +0200

    list articles with keywords

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ import httplib2 as http
 import urllib
 import json
 import feedparser
+import sys
 
 try:
     from urlparse import urlparse
@@ -12,6 +13,10 @@ headers = {
     'Accept': 'application/json',
     'Content-Type': 'application/json; charset=UTF-8'
 }
+
+def notify(msg):
+    sys.stderr.write(msg + "\n")
+    sys.stderr.flush()
 
 def extractEntities(url):
     uri = 'http://access.alchemyapi.com'
@@ -61,5 +66,27 @@ def extractKeywords(url):
 
     return json.loads(content)['keywords']
 
+def getAnnotatedArticles():
+    feedUrls = ['http://rss.cnn.com/rss/edition.rss']
+    articles = []
+    for feedUrl in feedUrls:
+        notify("parsing " + feedUrl)
+        feed = feedparser.parse(feedUrl)
+        for entry in feed['entries']:
+            article = {
+                'link': entry['link'],
+                'title': entry['title'],
+                'published': entry['published'],
+                'sourceUrl': feedUrl,
+                'source': feed['feed']['title']
+            }
+            notify("extracting keywords from " + entry['link'])
+            article['keywords'] = extractKeywords(entry['link'])
+            articles.append(article)
+    return articles
+
+
+
 #print(extractKeywords('http://edition.cnn.com/2013/06/26/world/meast/israel-settlement-kerry/index.html'))
-print(json.dumps(feedparser.parse('http://rss.cnn.com/rss/edition.rss')['entries']))
+#print(json.dumps(feedparser.parse('http://rss.cnn.com/rss/edition.rss')))
+print(json.dumps(getAnnotatedArticles()))
