6aaf9f822b368f550e98b39bda0f3c6bb3b13d90
Michael Limiero <mike5713@gmail.com>
2013-06-28 00:34:38 +0200

    sources, cities, and countries

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -20,10 +20,62 @@ function keywordWeights(list) {
     return keywords;
 }
 
-function articlesWithKeyword(list, keyword) {
+function filterByKeyword(list, keyword) {
     return _.filter(list, function(article) {
         return (_.find(article.keywords, function(k) {
             return k.text == keyword;
         })) != undefined;
+    });
+}
+
+function sourcesFromArticles(list) {
+    return _.object(_.uniq(_.map(list, function(article) {
+        return [article.source, article.sourceUrl];
+    })));
+}
+
+function filterBySource(list, source) {
+    return _.filter(list, function(article) {
+        return article.source == source;
+    });
+}
+
+function cityWeights(list) {
+    var cities = {};
+    _.each(list, function(article) {
+        _.each(article.cities, function(city) {
+                   if (cities[city]) {
+                       cities[city] += 1;
+                   } else {
+                       cities[city] = 1;
+                   }
+               });
+    });
+    return cities;
+}
+
+function countryWeights(list) {
+    var countries = {};
+    _.each(list, function(article) {
+        _.each(article.countries, function(country) {
+                   if (countries[country]) {
+                       countries[country] += 1;
+                   } else {
+                       countries[country] = 1;
+                   }
+               });
+    });
+    return countries;
+}
+
+function filterByCity(list, city) {
+    return _.filter(list, function(article) {
+        return _.contains(article.cities, city);
+    });
+}
+
+function filterByCountry(list, country) {
+    return _.filter(list, function(article) {
+        return _.contains(article.countries, country);
     });
 }
