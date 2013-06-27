31a90202d23d7a92a8fe852ba7d17325c80352a0
Michael Limiero <mike5713@gmail.com>
2013-06-27 23:00:44 +0200

    stuff

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 import httplib2 as http
+import urllib
 import json
+import feedparser
 
 try:
     from urlparse import urlparse
@@ -11,22 +13,53 @@ headers = {
     'Content-Type': 'application/json; charset=UTF-8'
 }
 
-uri = 'http://access.alchemyapi.com'
-path = '/calls/url/URLGetRankedNamedEntities?url=http://edition.cnn.com/2013/06/26/world/meast/israel-settlement-kerry/index.html&apikey=1bfa1ea9bb37171aa55db5eedcf90eb1d719c6bb&outputMode=json'
+def extractEntities(url):
+    uri = 'http://access.alchemyapi.com'
+    path = '/calls/url/URLGetRankedNamedEntities'
+    query = '?' + urllib.parse.urlencode({
+        'url': url,
+        'apikey': '1bfa1ea9bb37171aa55db5eedcf90eb1d719c6bb',
+        'outputMode': 'json'
+    })
 
-target = urlparse(uri+path)
-method = 'GET'
-body = ''
+    target = urlparse(uri+path+query)
+    method = 'GET'
+    body = ''
 
-h = http.Http()
+    h = http.Http()
 
-response, content = h.request(
-        target.geturl(),
-        method,
-        body,
-        headers)
-content = content.decode('utf-8')
+    response, content = h.request(
+            target.geturl(),
+            method,
+            body,
+            headers)
+    content = content.decode('utf-8')
 
-data = json.loads(content)['entities']
+    return json.loads(content)['entities']
 
-print(data)
+def extractKeywords(url):
+    uri = 'http://access.alchemyapi.com'
+    path = '/calls/url/URLGetRankedKeywords'
+    query = '?' + urllib.parse.urlencode({
+        'url': url,
+        'apikey': '1bfa1ea9bb37171aa55db5eedcf90eb1d719c6bb',
+        'outputMode': 'json'
+    })
+
+    target = urlparse(uri+path+query)
+    method = 'GET'
+    body = ''
+
+    h = http.Http()
+
+    response, content = h.request(
+            target.geturl(),
+            method,
+            body,
+            headers)
+    content = content.decode('utf-8')
+
+    return json.loads(content)['keywords']
+
+#print(extractKeywords('http://edition.cnn.com/2013/06/26/world/meast/israel-settlement-kerry/index.html'))
+print(json.dumps(feedparser.parse('http://rss.cnn.com/rss/edition.rss')['entries']))
